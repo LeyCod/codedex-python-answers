c1b87a25cd1ae5e3afd9e7a6edd63728e540141f
LeyCod <emmanuel_lg34@hotmail.com>
2022-10-13 18:24:56 +0200

    complete lists solutions

---
+++ lists/bucket_list.py
@@ -0,0 +1,15 @@
+things_to_do = [
+    '🚀 Build a meaningful product for everyone.',
+    '⛰ Hike the Pacific Crest Trail.',
+    '🏡 Build an A-frame house and raise some goats.',
+    '🌏 Live somewhere in Asia for a year.',
+    '🎸 Release an album.',
+    '📝 Write a book.',
+    '🏆 Reach 100k subscribers on YouTube.',
+    '🚐 Road trip with the fam.',
+    '🍳 Open a cozy diner upstate.',
+    '👴🏻 Grow old with no regrets.'
+] 
+
+for thing in things_to_do: 
+    print(thing)
